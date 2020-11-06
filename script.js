9dd54cbd87288ef1a17adbd29ace9bd3e4af56b3
Julio Nepomuceno <julioreplay@gmail.com>
2020-11-06 19:31:28 -0300

    Tela de login pronta

---
+++ script.js
@@ -0,0 +1,19 @@
+const labelEmail = document.getElementById("labelEmail")
+const labelPassword = document.getElementById("labelPassword")
+
+function clickEmail() {
+    labelEmail.style.color= "#93b3e4"
+}
+
+function clickEmailOver() {
+    labelEmail.style.color= "#c1c1c1"
+}
+
+function clickPassword() {
+    labelPassword.style.color= "#93b3e4"
+}
+
+function clickPasswordOver() {
+    labelPassword.style.color= "#c1c1c1"
+}
+
